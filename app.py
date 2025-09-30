be1956d0e52223e0d69ecdec3add97458a640539
muhammadjareer24 <m.ahmar2496@gmail.com>
2025-09-30 14:54:15 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, redirect
+import os
+import mysql.connector
+
+app = Flask(__name__)
+
+conn = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "24962496",
+    database = "library_db"
+)
+
+print("Connected to library_db")
+
+@app.route("/")
+def index():
+    cursor = conn.cursor(dictionary=True) # dictionary=True -> returns rows as dicts
+    cursor.execute("SELECT * FROM books")
+    books = cursor.fetchall()
+    # print(books)
+
+    cursor.close()
+    return render_template("index.html", books=books) 
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    author = request.form.get("author")
+    status = request.form.get("status")
+
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO books (title, author, status) VALUES (%s, %s, %s)", 
+        (title, author, status)
+    )
+    
+    conn.commit() #Save changes
+    cursor.close()
+    return redirect("/")
+
+
+@app.route("/delete/<int:book_id>", methods=["POST"])
+def delete(book_id):
+
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM books WHERE id = %s", (book_id,))
+
+    conn.commit()
+    cursor.close()
+    return redirect("/")
+
+
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
